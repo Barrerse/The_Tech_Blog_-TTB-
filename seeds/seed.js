d760a33d104d7457261a1538d9958d79f1b21e73
Barrerse <sebastianbarrera@live.com>
2023-03-05 14:25:14 -0500

    Fixed old seeds

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,9 @@
 const sequelize = require('../config/connection');
-const { User, Project } = require('../models');
+const { User, Post, Comment } = require('../models');
 
-const userData = require('./userData.json');
-const projectData = require('./projectData.json');
+const userData = require('./user-seeds.json');
+const postData = require('./post-seeds.json');
+const commentData = require('./comment-seeds.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -12,12 +13,20 @@ const seedDatabase = async () => {
     returning: true,
   });
 
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
+  const posts = await Post.bulkCreate(
+    postData.map(post => ({
+      ...post,
       user_id: users[Math.floor(Math.random() * users.length)].id,
-    });
-  }
+    }))
+  );
+
+  const comments = await Comment.bulkCreate(
+    commentData.map(comment => ({
+      ...comment,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+      post_id: posts[Math.floor(Math.random() * posts.length)].id,
+    }))
+  );
 
   process.exit(0);
 };
