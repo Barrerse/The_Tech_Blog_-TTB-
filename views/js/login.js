6df9fd12d74068d074fd14a736df8526342f56b7
Barrerse <sebastianbarrera@live.com>
2023-03-05 13:42:20 -0500

    fixed some login issues. changed handlebars to match techblog

--- views/js/login.js
+++ views/js/login.js
@@ -1,34 +1,34 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User } = require('../models');
 
-// POST /api/users/login
 router.post('/login', async (req, res) => {
   try {
-    // Find the user who matches the provided email address
+    // Find the user with the provided email address
     const userData = await User.findOne({ where: { email: req.body.email } });
 
+    // If the user doesn't exist, send a 400 Bad Request response
     if (!userData) {
       res.status(400).json({ message: 'Incorrect email or password, please try again' });
       return;
     }
 
-    // Check the provided password against the hashed password in the database
+    // Check if the provided password matches the stored password hash
     const validPassword = await userData.checkPassword(req.body.password);
 
+    // If the password doesn't match, send a 400 Bad Request response
     if (!validPassword) {
       res.status(400).json({ message: 'Incorrect email or password, please try again' });
       return;
     }
 
-    // Save the user's session so they can stay logged in
+    // Save the user's session data
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
+
       res.json({ user: userData, message: 'You are now logged in!' });
     });
-
   } catch (err) {
-    console.log(err);
     res.status(500).json(err);
   }
 });
